89b387604e7a83114ef6a5f45220407d9915ad0f
정진호 <jeongjinho@jeongjinhoui-MacBookAir.local>
2024-04-11 15:28:26 +0900

    scroll bar css added

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -94,6 +94,18 @@ const Area = styled.div<IAreaProps>`
   min-height: 80px;
   max-height: calc(100% - 200px);
   overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #999;
+    border-radius: 3px;
+    background-clip: padding-box;
+    border: 1px solid transparent;
+    transition: background-color 0.3s;
+  }
 `;
 
 const Form = styled.form`
